5903e703e09e40ac1a1c2255887eae9e3afe264b
Sam Sawchuk <samsawchuk@shaw.ca>
2015-06-23 15:54:51 -0400

    change made to show.html.erb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,29 +1,34 @@
 class PicturesController < ApplicationController
-	def index
-		@pictures = Picture.all
-	end
+  def index
+    @pictures = Picture.all
+  end
 
-	def show
-		@picture = Picture.find(params[:id])
-	end
+  def show
+    @picture = Picture.find(params[:id])
+  end
 
-	def new
-		@picture = Picture.new
-	end
+  def new
+    @picture = Picture.new
+  end
 
-	def create
-		@picture = Picture.new(picture_params)
-		if @picture.save
-			redirect_to pictures_url
-		else
-			render :new
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
 
-	def edit
-    	@picture = Picture.find(params[:id])
-  	end
+  def edit
+    @picture = Picture.find(params[:id])
+  end
 
-  	def update
-    	@picture = Picture.find(params[:id])
+  def update
+    @picture = Picture.find(params[:id])
 
     if @picture.update_attributes(picture_params)
       redirect_to "/pictures/#{@picture.id}"
@@ -31,19 +36,9 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
-end
 
-
-
-
-end
-
-
-end
-
-private
-def picture_params
-	params.require(:picture).permit(:artist, :title, :url)
-end
-
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
 end
